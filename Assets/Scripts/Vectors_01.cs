1033cdbebe6c190cb2025dd1af90204152060238
Wannacr7 <88002544+Wannacr7@users.noreply.github.com>
2021-08-30 13:35:57 -0500

    TeleportInsideTheLimits

--- Assets/Scripts/Vectors_01.cs
+++ Assets/Scripts/Vectors_01.cs
@@ -160,4 +160,5 @@ public class Vectors_01
     {
          return new Vector2(v.compX,v.compY);
     }
+    
 }
--- Assets/Scripts/Walker.cs
+++ Assets/Scripts/Walker.cs
@@ -6,54 +6,68 @@ public class Walker : MonoBehaviour
 {
     [SerializeField] Vectors_01 firstPosition;
     [SerializeField] Vectors_01 velocity;
+    [SerializeField] Vectors_01 Aceleration;
 
     Vectors_01 sum;
     Vectors_01 rest;
     Vectors_01 tool = new Vectors_01();
 
-    [SerializeField]Transform target;
+    [SerializeField]GameObject target;
     [SerializeField] int seed;
     [SerializeField] bool Automatic = false;
     // Start is called before the first frame update
     void Start()
     {
-        target.position = new Vector2(0f, 0f);
+        target.transform.position = new Vector2(0f, 0f);
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        firstPosition = new Vectors_01(target.position.x, target.position.y);
+        firstPosition = new Vectors_01(target.transform.position.x, target.transform.position.y);
         firstPosition.DrawVector();
 
         sum = firstPosition.Substraction2(velocity);
 
         velocity.DrawVector(firstPosition,Color.red);
 
+        target.transform.position = target.transform.position + velocity.ScalarMultiply2(Time.deltaTime);
+        velocity.Addition(Aceleration.ScalarMultiply2(Time.deltaTime));
+
+
         UpdatePosition();
     }
     public void UpdatePosition()
     {
 
-        target.position += velocity.ScalarMultiply2(Time.deltaTime);
 
-        if(target.position.x  > 4.5f)
+        if(target.transform.position.x  >= 4.5f)
         {
-            velocity.compX *= -1f;
+            
+            velocity.compX *= -1f ;
+            Aceleration.compX *= -1f;
+            target.transform.position = new Vector3(4.5f,0);
         }
-        if (target.position.x  < -4.5f)
+        else if (target.transform.position.x  <= -4.5f)
         {
-            velocity.compX *= -1f;
+            velocity.compX *= -1f ;
+            Aceleration.compX *= -1f;
+            target.transform.position = new Vector3(-4.5f, 0);
         }
-        if (target.position.y > 4.5f)
+        else if (target.transform.position.y >= 4.5f)
         {
-            velocity.compY *= -1f;
+            velocity.compY *= -1f ;
+            Aceleration.compY *= -1f;
+            target.transform.position = new Vector3(0, 4.5f);
         }
-        if (target.position.y < -4.5f)
+        else if (target.transform.position.y <= -4.5f)
         {
-            velocity.compY *= -1f;
+            velocity.compY *= -1f ;
+            Aceleration.compY *= -1f;
+            target.transform.position = new Vector3(0, -4.5f);
         }
+        
         Debug.Log("Update position");
     }
 }
